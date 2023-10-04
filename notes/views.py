8e6a28657097ca00bbeaec0fa418de5aff24ef7b
datttrian <datttrian@gmail.com>
2023-10-04 18:19:23 +0000

    Display content of a single note

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from .models import Notes
 
 
@@ -7,3 +7,11 @@ from .models import Notes
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+def detail(request, pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist")
+    return render(request, 'notes/notes_detail.html', {'note': note})
