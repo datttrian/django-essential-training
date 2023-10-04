f83be4587b236800121b2981e2ae84d1a1b527e7
datttrian <datttrian@gmail.com>
2023-10-04 17:45:39 +0000

    User authentication in two simple steps

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('home', views.home)]
+urlpatterns = [path('home', views.home), path('authorized', views.authorized)]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
