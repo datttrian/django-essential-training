8d3a1cee5f85079a189eac9bd864170fc293a0aa
datttrian <datttrian@gmail.com>
2023-10-04 20:39:31 +0000

    Adding a signup page

--- home/urls.py
+++ home/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     # path('authorized', views.AuthorizedView.as_view()),
     path('login', views.LoginInterfaceView.as_view(), name='login'),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('signup', views.SignupView.as_view(), name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,14 +1,28 @@
 from datetime import datetime
-from django.shortcuts import render
-from django.http import HttpResponse
 
+# from django.shortcuts import render
+# from django.http import HttpResponse
 # from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+
+
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
 
